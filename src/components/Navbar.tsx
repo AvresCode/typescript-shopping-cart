b10f95e9f91ddb97739bf6f510cd0e8ea35a25c5
AvresCode <servas230@gmail.com>
2022-10-29 23:18:01 +0200

    Added navbar items and shopping cart icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,29 @@
-import { Navbar as NavbarBS, Container } from "react-bootstrap";
-
+import { Navbar as NavbarBS, Container, Nav, Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
 export function Navbar() {
   return (
     <NavbarBS className="bg-white shadow-sm mb-3">
-      <Container> navbar</Container>
+      <Container>
+        <Nav>
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+        </Nav>
+        <Button
+          style={{ width: "2.5rem", height: "2.5rem" }}
+          variant="outline-primary"
+          className="rounded-circle"
+        >
+          <FaShoppingCart />
+        </Button>
+      </Container>
     </NavbarBS>
   );
 }
