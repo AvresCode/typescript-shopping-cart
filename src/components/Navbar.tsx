582cc32ca46ee10c6ad5ff65b47602e9777992fc
AvresCode <servas230@gmail.com>
2022-10-30 00:04:17 +0200

    Added div for number of items in shopping cart and styled it

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 import { FaShoppingCart } from "react-icons/fa";
 export function Navbar() {
   return (
-    <NavbarBS className="bg-white shadow-sm mb-3">
+    <NavbarBS sticky="top" className="bg-white shadow-sm mb-3">
       <Container>
         <Nav>
           <Nav.Link to="/" as={NavLink}>
@@ -17,11 +17,27 @@ export function Navbar() {
           </Nav.Link>
         </Nav>
         <Button
-          style={{ width: "2.5rem", height: "2.5rem" }}
+          style={{ width: "2.5rem", height: "2.5rem", position: "relative" }}
           variant="outline-primary"
           className="rounded-circle"
         >
           <FaShoppingCart />
+
+          <div
+            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+            style={{
+              color: "white",
+              width: "1.3rem",
+              height: "1.3rem",
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              transform: "translate(35%, 35%)",
+            }}
+          >
+            {" "}
+            3{" "}
+          </div>
         </Button>
       </Container>
     </NavbarBS>
