839ff7c3c2f9d777b35d4912cdd9309d2064b14d
AvresCode <servas230@gmail.com>
2022-11-01 23:07:58 +0100

    Added function to get the items in the navbar shopping cart

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,10 @@
 import { Navbar as NavbarBS, Container, Nav, Button } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import { FaShoppingCart } from "react-icons/fa";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+
 export function Navbar() {
+  const { openCart, cartQuantity } = useShoppingCart();
   return (
     <NavbarBS sticky="top" className="bg-white shadow-sm mb-3">
       <Container>
@@ -16,29 +19,33 @@ export function Navbar() {
             Store
           </Nav.Link>
         </Nav>
-        <Button
-          style={{ width: "2.5rem", height: "2.5rem", position: "relative" }}
-          variant="outline-primary"
-          className="rounded-circle"
-        >
-          <FaShoppingCart />
+        {/* if there is no item in shopping cart, it does not appear*/}
 
-          <div
-            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
-            style={{
-              color: "white",
-              width: "1.3rem",
-              height: "1.3rem",
-              position: "absolute",
-              bottom: 0,
-              right: 0,
-              transform: "translate(35%, 35%)",
-            }}
+        {cartQuantity > 0 && (
+          <Button
+            onClick={openCart}
+            style={{ width: "2.5rem", height: "2.5rem", position: "relative" }}
+            variant="outline-primary"
+            className="rounded-circle"
           >
-            {" "}
-            3{" "}
-          </div>
-        </Button>
+            <FaShoppingCart />
+
+            <div
+              className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+              style={{
+                color: "white",
+                width: "1.3rem",
+                height: "1.3rem",
+                position: "absolute",
+                bottom: 0,
+                right: 0,
+                transform: "translate(35%, 35%)",
+              }}
+            >
+              {cartQuantity}
+            </div>
+          </Button>
+        )}
       </Container>
     </NavbarBS>
   );
--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -12,10 +12,14 @@ type CartItem = {
 };
 //item quantity,  increment, decrement, remove
 type ShoppingCartContext = {
+  openCart: () => void;
+  closeCart: () => void;
   getItemQuantity: (id: number) => number;
   increaseCartQuantity: (id: number) => void;
   decreaseCartQuantity: (id: number) => void;
   removeFromCart: (id: number) => void;
+  cartQuantity: number;
+  cartItems: CartItem[];
 };
 
 const ShoppingCartContext = createContext({} as ShoppingCartContext);
@@ -29,6 +33,16 @@ export function useShoppingCart() {
 
 export function ShoppingCartProvider({ children }: ShoppingCartProvideProps) {
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  //
+  const cartQuantity = cartItems.reduce(
+    (quantity, item) => item.quantity + quantity,
+    0
+  );
+
+  const openCart = () => setIsOpen(true);
+  const closeCart = () => setIsOpen(false);
 
   function getItemQuantity(id: number) {
     return cartItems.find((item) => item.id === id)?.quantity || 0;
@@ -82,6 +96,10 @@ export function ShoppingCartProvider({ children }: ShoppingCartProvideProps) {
         increaseCartQuantity,
         decreaseCartQuantity,
         removeFromCart,
+        openCart,
+        closeCart,
+        cartItems,
+        cartQuantity,
       }}
     >
       {children}
